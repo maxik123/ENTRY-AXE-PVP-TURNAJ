24bc5a18fd1dce628034306f37ec59eefef5bf83
maxik123 <68480851+maxik123@users.noreply.github.com>
2026-08-04 22:43:30 +0200

    Refactor players array and simplify player entries

--- js/players.js
+++ js/players.js
@@ -1,81 +1,34 @@
 const players = [
 
-{
-name:"Lukyng",
-rank:"LT1"
-},
+{name:"Lukyng",rank:"LT1"},
+{name:"Mathhew",rank:"LT1"},
+{name:"Kufrajz",rank:"LT1"},
 
-{
-name:"Mathhew",
-rank:"LT1"
-},
+{name:"Ze1ro",rank:"HT2"},
+{name:"Caisem",rank:"HT2"},
 
-{
-name:"Kufrajz",
-rank:"LT1"
-},
+{name:"zeuxi",rank:"LT2"},
+{name:"Fleyz",rank:"LT2"},
+{name:"Siwi",rank:"LT2"},
 
+{name:"Predator",rank:"HT3"},
+{name:"Silvestr",rank:"HT3"},
 
-{
-name:"Ze1ro",
-rank:"HT2"
-},
-
-{
-name:"Caisem",
-rank:"HT2"
-},
-
-
-{
-name:"zeuxi",
-rank:"LT2"
-},
-
-{
-name:"Fleyz",
-rank:"LT2"
-},
-
-{
-name:"Siwi",
-rank:"LT2"
-},
-
-
-{
-name:"Predator",
-rank:"HT3"
-},
-
-{
-name:"Silvestr",
-rank:"HT3"
-},
-
-
-{
-name:"DarkHonzaCz",
-rank:"LT3"
-},
-
-{
-name:"Wbot",
-rank:"LT3"
-}
+{name:"DarkHonzaCz",rank:"LT3"},
+{name:"Wbot",rank:"LT3"}
 
 ];
 
 
 
-const container = document.getElementById("players");
+const box=document.getElementById("players");
 
 
 
 players.forEach(player=>{
 
 
-container.innerHTML += `
+box.innerHTML += `
 
 <div class="player-card">
 
@@ -98,6 +51,5 @@ ${player.rank}
 </div>
 
 `;
-
 
 });
