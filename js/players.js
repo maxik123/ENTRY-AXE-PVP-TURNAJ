f421cf0efd2cfb596c56eecc0735ca38dedd4575
maxik123 <68480851+maxik123@users.noreply.github.com>
2026-08-05 10:09:48 +0200

    Modify player details for MINER99
    Updated player description and added TikTok link.

--- js/players.js
+++ js/players.js
@@ -5,7 +5,7 @@ const players = [
     {
         name:"MINER99",
         rank:"LT1",
-        description:"",
+        description:"pridal bych tipovacku",
         upcoming:[
             "rnd0",
             "",
@@ -13,7 +13,7 @@ const players = [
         ],
         socials:{
             youtube:"https://youtube.com/@miner99?si=FnC7AGQAdDHFlMcl",
-            twitch:"",
+            tiktok:"https://www.tiktok.com/@miner99wasalreadytaken?_r=1&_t=ZN-98cl2DI7tjC",
             instagram:"https://www.instagram.com/miner99cz?igsh=MzlqbWo1MHBobHdx&utm_source=qr",
             discord:"https://discord.gg/DCvapNsQY"
             
