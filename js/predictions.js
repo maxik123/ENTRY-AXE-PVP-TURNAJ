ea972895b225f9c694741f5702e8c2b0119f6411
maxik123 <68480851+maxik123@users.noreply.github.com>
2026-08-13 22:54:06 +0200

    Update player2 in match data for predictions

--- js/predictions.js
+++ js/predictions.js
@@ -27,7 +27,7 @@ const matches = [
     {
         id:"xdanicz-drticka",
         player1:"xDaniCz",
-        player2:"Drticka",date:"?. ?. 2026",time:"?",stream:"https://www.youtube.com/live/K2_0Oq3q9n0"
+        player2:"WBotWarrior7",date:"15. 8. 2026",time:"13:00",stream:"https://www.youtube.com/live/K2_0Oq3q9n0"
     },
         {
         id:"argys-pleistonax",
