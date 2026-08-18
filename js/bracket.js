6050e0a2d759f8840d8cd9ff558ff239b6b988fc
maxik123 <68480851+maxik123@users.noreply.github.com>
2026-08-19 01:51:47 +0200

    Update score for GROUP1_LB_R1

--- js/bracket.js
+++ js/bracket.js
@@ -82,7 +82,7 @@ const GROUP1_WB_FINAL = {
 // vítěz WB finále vs vítěz LB finále
 
 const GROUP1_LB_R1 = [
-    { score1: null, score2: null, streams: [] },
+    { score1: 7, score2: 20, streams: [] },
     { score1: null, score2: null, streams: [] }
 ];
 
