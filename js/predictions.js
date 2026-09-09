b80fa1ac3202b323e31b79b2c65544cfd66824cf
maxik123 <68480851+maxik123@users.noreply.github.com>
2026-09-09 16:41:44 +0200

    Refactor predictions and update match data

--- js/predictions.js
+++ js/predictions.js
@@ -22,6 +22,16 @@ from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 const matches = [
     
     {
+        id:"drown-caisem",
+        player1:"caisem",
+        player2:"DroWnerT",date:"?. 9. 2026",time:"00:00",stream:""
+    }, 
+    {
+        id:"martyz-zeiro",
+        player1:"Ze1ro00",
+        player2:"xmartyz",date:"9. 9. 2026",time:"19:00",stream:""
+    }, 
+    {
         id:"housle-kufr",
         player1:"kufrajzcz",
         player2:"TenSHouslema",date:"7. 9. 2026",time:"19:00",stream:""
@@ -38,11 +48,6 @@ const matches = [
         player2:"Predator_18",date:"?. ?. 2026",time:"?",stream:""
     },   
 
-     {
-        id:"drown-retr",
-        player1:"Alpha_R3turns",
-        player2:"DroWnerT",date:"6. 9. 2026",time:"19:00",stream:""
-    },   
  
     
 ];
